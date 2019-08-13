f6ddd1c0d51393b06f5b7e74d36a97c49ce8b72a
brrcrites <brrcrites@gmail.com>
2019-08-13 23:50:59 +0100

    Add basic login flow

--- public/src/login.js
+++ public/src/login.js
@@ -1,18 +1,37 @@
 
-function initialize() {
-    var firebase = require('firebase');
-    var firebaseui = require('firebaseui');
+const firebase = require('firebase/app');
+const firebaseAuth = require('firebase/auth');
+const firebaseStore = require('firebase/firestore');
+
+const firebaseUi = require('firebaseui');
+
+//function initialize() {
 
     firebase.initializeApp(firebaseConfig);
 
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    ui.start('#firebaseui-auth-container', {
+    var ui = new firebaseUi.auth.AuthUI(firebase.auth());
+
+    var uiConfig = {
         signInOptions: [
             {
                 provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
                 requireDisplayName: true
             }
-        ]
-    });
-}
+        ],
+        callbacks: {
+            signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+                return true;
+            },
+            uiShown: function() {
+                document.getElementById('loader').style.display = 'none';
+            }
+        },
+        signInSuccessUrl: 'success.html',
+        tosUrl: '',
+        privacyPolicyUrl: ''
+    };
+
+    ui.start('#firebaseui-auth-container', uiConfig);
+
+//}
 
