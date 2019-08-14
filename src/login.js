bc0da674dbf2d263308a637ec7039420d1ab1d78
brrcrites <brrcrites@gmail.com>
2019-08-14 22:35:47 +0100

    Add user factory functionality and flow

--- src/login.js
+++ src/login.js
@@ -5,12 +5,29 @@ const firebaseStore = require('firebase/firestore');
 
 const firebaseUi = require('firebaseui');
 
-export function initialize() {
+firebase.initializeApp(firebaseConfig);
 
-    firebase.initializeApp(firebaseConfig);
+class User {
+    constructor(email, firstName, lastName, nickName, uid) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.uid = uid;
+    }
+}
 
+class Factory {
+    createUser(firstName, lastName, nickName) {
+        var user = firebase.auth().currentUser;
+        var email = user.email;
+        var uid = user.uid;
+        return new User(email, firstName, lastName, nickName, uid);
+    }
+}
+
+export function initializeAuth() {
     var ui = new firebaseUi.auth.AuthUI(firebase.auth());
-
     var uiConfig = {
         signInOptions: [
             {
@@ -32,6 +49,45 @@ export function initialize() {
     };
 
     ui.start('#firebaseui-auth-container', uiConfig);
+}
 
+export function mintUser() {
+    var userFactory = new Factory();
+
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var nickName = document.getElementById('nickName').value;
+    
+    var newUser = userFactory.createUser(firstName, lastName, nickName);
+
+    localStorage.setItem('user', JSON.stringify(newUser));
+    window.location.replace('userInfo.html');
+}
+
+export function loadUserInfo() {
+    var user = JSON.parse(localStorage.getItem('user'));
+    console.log(`Email:${user.email},First:${user.firstName},Last:${user.lastName},Nickname:${user.nickName},UID:${user.uid}`);
+
+    var userdiv = document.getElementById('userInfo');
+
+    var emailLiElement = document.createElement("LI");
+    var emailTextElement = document.createTextNode(`Email: ${user.email}`);
+    emailLiElement.appendChild(emailTextElement);
+    userdiv.appendChild(emailLiElement);
+
+    var firstLiElement = document.createElement("LI");
+    var firstTextElement = document.createTextNode(`First: ${user.firstName}`);
+    firstLiElement.appendChild(firstTextElement);
+    userdiv.appendChild(firstLiElement);
+
+    var lastLiElement = document.createElement("LI");
+    var lastTextElement = document.createTextNode(`Last: ${user.lastName}`);
+    lastLiElement.appendChild(lastTextElement);
+    userdiv.appendChild(lastLiElement);
+
+    var uidLiElement = document.createElement("LI");
+    var uidTextElement = document.createTextNode(`UID: ${user.uid}`);
+    uidLiElement.appendChild(uidTextElement);
+    userdiv.appendChild(uidLiElement);
 }
 
